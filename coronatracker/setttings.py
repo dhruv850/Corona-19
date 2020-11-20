c47827d18720e1e566e410847e5d0c0234b953f7
Dhruv Patel <dhruv850.dp@gmail.com>
2020-11-20 13:08:42 -0500

    empty

--- coronatracker/setttings.py
+++ coronatracker/setttings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sssr3bsl5k(&37vz6-i!8o90z79y9&wgle#t(#b)^&784(**%('
+SECRET_KEY = 'Enter Key Here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
